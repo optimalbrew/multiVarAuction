fdc9b6c4b073cb22587560b6e37674d6f0452712
Pete Carkeek <petecarkeek@gmail.com>
2019-04-08 13:21:25 -0700

    add small setup bash script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Auctions on ethereum 
 
 Basic setup on EC2 ubuntu to work with auction contracts on ethereum using truffle and ganache-cli.
+
+Set up script `myAWSsetup.sh` for use with Ubuntu 18.04 LTS on t2 micro. 
---
+++ myAWSsetup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Set up script for Ubuntu 18.04 LTS on AWS t2 micro instance
+
+sudo apt-get update
+
+echo 'Set up memory swap'
+# memory swap set up
+sudo dd if=/dev/zero of=/swapfile bs=1M count=512
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo chown root:root /swapfile
+sudo chmod 0600 /swapfile
+
+echo 'Installing node v10.15.3'
+## install node
+wget https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-x64.tar.xz
+tar -xf node-v10.15.3-linux-x64.tar.xz
+
+sudo mv node-v10.15.3-linux-x64 /usr/local/
+sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/node /usr/bin/node
+node --version
+
+## 
+sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/npm /usr/bin/npm
+sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/npx /usr/bin/npx
+
+npm --version
+npx --version
+
+
+## Install truffle
+echo 'Installing truffle'
+npm install -g truffle
+
+sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/truffle /usr/bin/truffle
+
+echo 'Print truffle version and components'
+truffle version
+
+# install ganache-cli
+echo 'Installing ganache-cli'
+
+npm install -g ganache-cli 
+
+sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/ganache-cli /usr/bin/ganache-cli
+
+# Also install openzep sol test
+echo 'Installing open zeppelin'
+
+npm install -g openzeppelin-solidity
+
