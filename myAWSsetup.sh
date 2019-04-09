e0ef9beff7d735d0e70aabac82a514372f3cc968
Pete Carkeek <petecarkeek@gmail.com>
2019-04-08 18:40:25 -0700

    added python and build-essential for later

--- myAWSsetup.sh
+++ myAWSsetup.sh
@@ -4,10 +4,11 @@
 # assumes repo has already been cloned so this setup script is available!
 # git clone https://github.com/petecarkeek/multiVarAuction.git  
 # cd multiVarAuction
+# chmod +x myAWSsetup.sh
 
 sudo apt-get update
 
-echo 'Set up memory swap'
+printf '\n\nSet up memory swap\n\n'
 # memory swap set up
 sudo dd if=/dev/zero of=/swapfile bs=1M count=512
 sudo mkswap /swapfile
@@ -15,7 +16,14 @@ sudo swapon /swapfile
 sudo chown root:root /swapfile
 sudo chmod 0600 /swapfile
 
-echo 'Installing node v10.15.3'
+
+printf '\n\ninstalling python2 and build-essential (for use in the future)\n\n'
+
+sudo apt.get install -y python
+
+sudo apt install -y build-essential
+
+printf '\n\nInstalling node v10.15.3\n\n'
 ## install node
 wget https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-x64.tar.xz
 tar -xf node-v10.15.3-linux-x64.tar.xz
@@ -33,23 +41,23 @@ npx --version
 
 
 ## Install truffle
-echo 'Installing truffle'
+printf '\n\nInstalling truffle\n\n'
 npm install -g truffle
 
 sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/truffle /usr/bin/truffle
 
-echo 'Print truffle version and components'
+printf '\n\nPrint truffle version and components\n\n'
 truffle version
 
 # install ganache-cli
-echo 'Installing ganache-cli'
+printf '\n\nInstalling ganache-cli\n\n'
 
 npm install -g ganache-cli 
 
 sudo ln -s /usr/local/node-v10.15.3-linux-x64/bin/ganache-cli /usr/bin/ganache-cli
 
 # Also install openzep sol test
-echo 'Installing open zeppelin'
+printf '\n\nInstalling open zeppelin\n\n'
 
 npm install -g openzeppelin-solidity
 
