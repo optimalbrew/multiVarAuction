7b46c5d400c459c5d6254d84d18fa7adc328c321
Pete Carkeek <petecarkeek@gmail.com>
2019-04-10 11:45:21 -0700

    modified test scripts

--- scripts/test1.js
+++ scripts/test1.js
@@ -1,19 +1,117 @@
 //Not a standard test. Just checking scripts as a way to interact with contracts.
 // single parameter callback function to run scripts using truffle exec
-module.exports = async function(finished){
-    const Auction = artifacts.require("./Auction.sol");
 
-    console.log('Here we go. Deploying instance..');
-    let instance = await Auction.deployed();    
+//Initial A+B procurement auction (Elliot Auction).
+module.exports = async function(finished){
+    const abAuction = artifacts.require("./abAuction.sol");
+
+    console.log('********* \nOkay, Here we go \n(Font size okay at the back?)\n');
+    let instance = await abAuction.deployed();    
     try {
         let accounts = await web3.eth.getAccounts();
+        
+        //parameters for contract constructor
+        let bidTime = 4;
+        let revealTime = 5;
+        let beneficiary = accounts[9];
+        let userCost = 2;  //the unit for this must match 'cost/time' e.g. $million/day       
+        let minDeposit = 2000000;
         //console.log(accounts);
-        instance = await Auction.new(60,30,accounts[9]);
+        instance = await abAuction.new(bidTime, revealTime, beneficiary, userCost, minDeposit);
+        
+        //time check
+        let tCheck1 = await instance.getCurrentTime();
+
         let contractAddress = instance.address;
-        console.log('New instance deployed at ' + contractAddress);
+        console.log('\n\x1b[32m%s\x1b[0m','New Auction contract instance deployed at ' + contractAddress);
         let contractBal = await web3.eth.getBalance(contractAddress);      
-        console.log('New instance has balance ' + contractBal + ' wei.');
-                
+        console.log('\nContract', contractAddress,'has balance ' + contractBal + ' wei.\n');
+        //console.log('\nNew instance has balance ' + contractBal + ' wei.\n');
+
+        console.log('\n\x1b[34m%s\x1b[0m','Create some sample bids \n********');
+        console.log('let bids = [[10,2], [13,1], [7,3], [8,2]];');
+        // let bidValue = [10,12,7];
+        // let days2Finish = [1,1,2];
+        // using the first elem is bid cost (or value) and the second elem is bid time
+        let bids = [[10,2], [13,1], [7,3], [8,2]];
+
+        //scoring function
+        function score(cost, time){return (cost + (time * userCost));}
+        //score the bids
+        let bidScores = [];
+        for (i=0;i<bids.length;i++){bidScores.push(score(bids[i][0],bids[i][1]));}
+
+        //let hashedBid = [encBid0,encBid1,encBid2];
+        let hashedBid = [];
+        for (i = 0; i < bids.length;i++){
+            hashedBid.push(web3.utils.soliditySha3({t:'uint256' , v: bids[i][0]}, {t: 'uint16', v: bids[i][1]}));
+        }
+        /* single version of above for easy copy pasting into truffle developer
+        for (i = 0; i < bids.length;i++){hashedBid.push(web3.utils.soliditySha3({t:'uint256' , v: bids[i][0]}, {t: 'uint16', v: bids[i][1]}));}
+        */
+
+        //print bids for illustration
+        console.log('The bids are encrypted. Displaying values for illustration.');
+        for (i = 0; i < bids.length;i++){
+            console.log('\n\nBidder',i+1, '@', accounts[i+1]);
+            console.log('Submits bid hash', hashedBid[i]);
+            console.log('bid cost: ', bids[i][0], ' & ', 'bid time:', bids[i][1], '(not revealed yet)');
+            console.log('Score: ', bidScores[i]);
+        }
+
+        console.log('\n*********\nRecall: lowest score wins.\n*******')
+        //single line version of above
+        //for (i = 0; i < bids.length;i++){console.log('Bidder @', accounts[i+1], 'Cost: ', bids[i][0], ' & ', bid[i][1], 'weeks with hash', hashedBid[i] );}
+
+        //Actual bidding (interaction with contract..)
+        console.log('\n\nDeposit (3 million wei assumed, minimum is 2 million) to place a bid. These funds are held in the contract.\n********');
+        console.log('\n\x1b[34m%s\x1b[0m','Balances before bidding.');
+        for (i = 0; i < bids.length;i++){
+            let balB4Bid = await web3.eth.getBalance(accounts[i+1]);  
+            console.log('Bidder',i+1, '@', accounts[i+1], 'has balance', balB4Bid);
+            instance.placeBid(hashedBid[i], {from: accounts[i+1],value: 3000000}); //deposit  identical as it should not reveal bid value 
+        }
+
+        //check balances after bidding
+        console.log('\n\x1b[34m%s\x1b[0m','Balances after bidding.');
+        for (i = 0; i < bids.length;i++){
+            let balAfterBid = await web3.eth.getBalance(accounts[i+1]);  
+            console.log('Bidder',i+1, '@', accounts[i+1], 'has balance', balAfterBid);
+        }
+
+        // contract balance    
+        contractBal = await web3.eth.getBalance(contractAddress);      
+        console.log('\n\x1b[34m%s\x1b[0m','Contract ' + contractAddress +' has balance ' + contractBal + ' wei.\n');
+
+        //get a time check
+        let tCheck2 = await instance.getCurrentTime();
+        //console.log('Time elapsed since instance created', tCheck2-tCheck1, 'sec.');
+
+        //moving on to bid revelation phase using the same bids as before (no need for new ones).
+        var check = async function(){
+            tCheck2 = await instance.getCurrentTime();
+            if((tCheck2 - tCheck1) > (bidTime + 1)){
+                // reveal bids when condition is met
+                console.log('\n******');
+                for (i=0; i< bids.length;i++){
+                    instance.revealBid(bids[i][0], bids[i][1], {from: accounts[i+1]});
+                    console.log('\n\x1b[32m%s\x1b[0m','Bid '+ (i+1) + ' revealed.');
+                }
+                //If the above worked then we will have a top bid
+                let winningBid = await instance.winBid();
+                console.log(winningBid);        
+
+            }
+            else {
+                console.log('\n\x1b[31m%s\x1b[0m','Bidding phase: Bids cannot be revealed yet.');
+                setTimeout(check, 2000); // check again in 2 secs
+            }
+        }
+        check();
+        
+        
+        // Finalize auction outcome
+
     } catch (error) {
         console.log(error.message);
     }
--- scripts/test2.js
+++ scripts/test2.js
@@ -23,23 +23,23 @@ module.exports = async function(finished){
         let tCheck1 = await instance.getCurrentTime();
 
         let contractAddress = instance.address;
-        console.log('\n\x1b[32m%s\x1b[0m','New Auction contract instance deployed at ' + contractAddress);
+        console.log('\n\x1b[32m%s\x1b[0m','New contract instance deployed at address' + contractAddress);
+        
         let contractBal = await web3.eth.getBalance(contractAddress);      
         console.log('\nContract', contractAddress,'has balance ' + contractBal + ' wei.\n');
-        //console.log('\nNew instance has balance ' + contractBal + ' wei.\n');
-
-        console.log('\n\x1b[34m%s\x1b[0m','Create some sample bids \n********');
-        console.log('let bids = [[10,2], [13,1], [7,3], [8,2]];');
-        // let bidValue = [10,12,7];
-        // let days2Finish = [1,1,2];
-        // using the first elem is bid cost (or value) and the second elem is bid time
+        
+        // bidding phase
+        console.log('\n\x1b[34m%s\x1b[0m','*********\nBidding Phase \n*********');
+        
         let bids = [[10,2], [13,1], [7,3], [8,2]];
+        console.log('Sample bids = [[10,2], [13,1], [7,3], [8,2]];');
 
         //scoring function
         function score(cost, time){return (cost + (time * userCost));}
         //score the bids
+        
         let bidScores = [];
-        for (i=0;i<bids.length;i++){bidScores.push(score(bids[i][0],bids[i][1]));}
+        for (i=0;i<bids.length;i++){bidScores.push(score(bids[i][0],bids[i][1]));} // should be done later (after bids revealed, btu okay for illustration)
 
         //let hashedBid = [encBid0,encBid1,encBid2];
         let hashedBid = [];
@@ -50,27 +50,20 @@ module.exports = async function(finished){
         for (i = 0; i < bids.length;i++){hashedBid.push(web3.utils.soliditySha3({t:'uint256' , v: bids[i][0]}, {t: 'uint16', v: bids[i][1]}));}
         */
 
+         //Actual bidding (interaction with contract..)
+         console.log('\n\nDeposit (3 million wei assumed, minimum is 2 million) to place a bid. These funds are held in the contract.\n********');
+         console.log('\n\x1b[34m%s\x1b[0m','Balances before bidding.');
+         for (i = 0; i < bids.length;i++){
+             let balB4Bid = await web3.eth.getBalance(accounts[i+1]);  
+             console.log('Bidder',i+1, '@', accounts[i+1], 'has balance', balB4Bid);
+             instance.placeBid(hashedBid[i], {from: accounts[i+1],value: 3000000}); //deposit should not reveal bid value (assumed same for now).
+         }
         //print bids for illustration
-        console.log('The bids are encrypted. Displaying values for illustration.');
+        //console.log('The bids are encrypted. Displaying values for illustration.');
         for (i = 0; i < bids.length;i++){
             console.log('\n\nBidder',i+1, '@', accounts[i+1]);
             console.log('Submits bid hash', hashedBid[i]);
-            console.log('bid cost: ', bids[i][0], ' & ', 'bid time:', bids[i][1], '(not revealed yet)');
-            console.log('Score: ', bidScores[i]);
-        }
-
-        console.log('\n*********\nRecall: lowest score wins.\n*******')
-        //single line version of above
-        //for (i = 0; i < bids.length;i++){console.log('Bidder @', accounts[i+1], 'Cost: ', bids[i][0], ' & ', bid[i][1], 'weeks with hash', hashedBid[i] );}
-
-        //Actual bidding (interaction with contract..)
-        console.log('\n\nDeposit (3 million wei assumed, minimum is 2 million) to place a bid. These funds are held in the contract.\n********');
-        console.log('\n\x1b[34m%s\x1b[0m','Balances before bidding.');
-        for (i = 0; i < bids.length;i++){
-            let balB4Bid = await web3.eth.getBalance(accounts[i+1]);  
-            console.log('Bidder',i+1, '@', accounts[i+1], 'has balance', balB4Bid);
-            instance.placeBid(hashedBid[i], {from: accounts[i+1],value: 3000000}); //deposit  identical as it should not reveal bid value 
-        }
+        }    
 
         //check balances after bidding
         console.log('\n\x1b[34m%s\x1b[0m','Balances after bidding.');
@@ -83,9 +76,13 @@ module.exports = async function(finished){
         contractBal = await web3.eth.getBalance(contractAddress);      
         console.log('\n\x1b[34m%s\x1b[0m','Contract ' + contractAddress +' has balance ' + contractBal + ' wei.\n');
 
+
         //get a time check
         let tCheck2 = await instance.getCurrentTime();
         //console.log('Time elapsed since instance created', tCheck2-tCheck1, 'sec.');
+
+        // bidding phase
+        console.log('\n\x1b[34m%s\x1b[0m','*********\nTime to reveal bids yet? \n*********');
 
         //moving on to bid revelation phase using the same bids as before (no need for new ones).
         var check = async function(){
@@ -97,9 +94,21 @@ module.exports = async function(finished){
                     instance.revealBid(bids[i][0], bids[i][1], {from: accounts[i+1]});
                     console.log('\n\x1b[32m%s\x1b[0m','Bid '+ (i+1) + ' revealed.');
                 }
-                //If the above worked then we will have a top bid
-                let winningBid = await instance.winBid();
-                console.log(winningBid);        
+                
+                //After bids revealed, show what they are and the result 
+                // scoring phase
+                console.log('\n*********\nRecall: Scoring phase: lowest score wins!!\n*******')
+        
+                    for (i = 0; i < bids.length;i++){
+                        console.log('\n\nBidder',i+1, '@', accounts[i+1]);
+                        console.log('bid cost: ', bids[i][0], ' & ', 'bid time:', bids[i][1], '(not revealed yet)');
+                        console.log('Score: ', bidScores[i]);
+                    }
+        
+                    // Fetch winning bid
+                    let winningBid = await instance.winBid();
+                    console.log('\nThe winning bid is\n') 
+                    console.log(winningBid);  
 
             }
             else {
@@ -107,9 +116,11 @@ module.exports = async function(finished){
                 setTimeout(check, 2000); // check again in 2 secs
             }
         }
+        // Check if it is time to reveal.
         check();
         
-        
+              
+
         // Finalize auction outcome
 
     } catch (error) {
